b0bf9f65f94df27a214472a567be55e658a12246
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-08-24 22:42:33 +0200

    prep

--- src/patterns/gitSkipRule.ts
+++ src/patterns/gitSkipRule.ts
@@ -5,11 +5,11 @@ let cachedGitSkipRule: SkipRule | null = null
 /**
  * Creates and caches a {@link SkipRule} that skips `.git` directories.
  *
- * @since 0.12.4
+ * @since 0.13.0
  */
 export function makeGitSkipRule(): SkipRule {
-	return (cachedGitSkipRule ||= (options) => {
-		if (options.dirent.isDirectory() && options.dirent.name === ".git") return 0
+	return (cachedGitSkipRule ||= ({ dirent }) => {
+		if (dirent.isDirectory() && dirent.name === ".git") return 0
 		return null
 	})
 }
